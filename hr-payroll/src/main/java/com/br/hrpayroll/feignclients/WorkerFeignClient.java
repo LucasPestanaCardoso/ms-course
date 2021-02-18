e26a183d1c4d2b53b76a9ebcf49c55fa3d380a45
LucasPestanaCardoso <lucaspestanaa@gmail.com>
2021-02-17 22:18:21 -0300

    Ribbon load balance

--- hr-payroll/src/main/java/com/br/hrpayroll/feignclients/WorkerFeignClient.java
+++ hr-payroll/src/main/java/com/br/hrpayroll/feignclients/WorkerFeignClient.java
@@ -10,7 +10,7 @@ import com.br.hrpayroll.entities.Worker;
 
 
 @Component
-@FeignClient(name = "hr-worker", url = "localhost:8001", path = "/workers")
+@FeignClient(name = "hr-worker",  path = "/workers")
 public interface WorkerFeignClient {
 
 	@GetMapping(value = "/{id}")
