da61cbf0dee6fdd51f2ae916c6ea14a1c677b6e8
LucasPestanaCardoso <lucaspestanaa@gmail.com>
2021-02-19 21:46:14 -0300

    Criacao das tabelas

---
+++ hr-user/src/main/java/com/br/hruser/entities/User.java
@@ -0,0 +1,48 @@
+package com.br.hruser.entities;
+
+import java.io.Serializable;
+import java.util.Set;
+
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.JoinTable;
+import javax.persistence.ManyToMany;
+import javax.persistence.Table;
+
+import com.google.common.collect.Sets;
+
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@EqualsAndHashCode
+
+@Entity
+@Table(name = "tb_user")
+public class User implements Serializable {
+
+	
+	private static final long serialVersionUID = 1L;
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	private String name;
+	private String email;
+	private String password;
+	
+	@ManyToMany(fetch = FetchType.EAGER)
+	@JoinTable(name = "tb_user_role", 
+	  joinColumns = @JoinColumn(name = "user_id") , 
+	  inverseJoinColumns =  @JoinColumn(name = "role_id"))
+	private Set<Role> roles = Sets.newHashSet();
+ 	
+}
